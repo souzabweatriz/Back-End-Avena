1e939dbee28b747ccb9d967e556e2a50c17c402f
Bea <ab291012@gmail.com>
2025-09-25 11:54:11 -0300

    refactor: folder structure

--- src/routes/categoriaRoutes.js
+++ src/routes/categoriaRoutes.js
@@ -1,1 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const categoriaController = require("../controllers/categoriaController");
 
+router.get("/categorias", categoriaController.getAllCategorias);
+router.get("/categorias/:id", categoriaController.getCategoriaById);
+
+module.exports = router;
--- src/routes/remedioRoutes copy.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const categoriaController = require("../controllers/categoriaController");
-
-router.get("/categorias", categoriaController.getAllCategorias);
-router.get("/categorias/:id", categoriaController.getCategoriaById);
-
-module.exports = router;
